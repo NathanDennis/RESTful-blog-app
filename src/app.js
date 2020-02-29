38e7ecde0409a51664dc75abc4c7cb7db915998f
NathanDennis <nathdennis@googlemail.com>
2020-02-29 07:03:40 -0500

    added all 7 RESTful routes, edit post page & edit/delete functionality

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const dotenv = require('dotenv')
 dotenv.config()
 const { PORT } = process.env
@@ -9,6 +10,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 
 // mongoose setup
 const mongoose = require('mongoose')
@@ -32,16 +34,70 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
     Blog.find({}, (error, posts) => {
         if (error) {
-            res.status(400).send('404 - Post not found')
+            res.status(400).send('404 - Posts not found')
         }
 
         res.render('index', {posts})
     })
 })
 
-// create route
+// create routes
 app.get('/posts/new', (req, res) => {
     res.render('new')
+})
+
+app.post('/posts', (req, res) => {
+    Blog.create(req.body.post, (error, newPost) => {
+        if (error) {
+            res.render('new')
+        }
+
+        res.redirect('/posts')
+    })
+})
+
+// show full post
+app.get('/posts/:id', (req, res) => {
+    Blog.findById(req.params.id, (error, post) => {
+        if(error) {
+            res.redirect('/posts')
+        }
+        res.render('show', {post})
+    })
+})
+
+// edit post
+app.get('/posts/:id/edit', (req, res) => {
+    Blog.findById(req.params.id, (error, post) => {
+        if(error) {
+            res.redirect('/posts')
+        }
+
+        res.render('edit', {post})
+    })
+})
+
+// update post
+app.put('/posts/:id', (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.post, (error, updatedPost) => {
+        if (error) {
+            res.redirect('/posts')
+        }
+
+        res.redirect(`/posts/${req.params.id}`)
+    })
+})
+
+// delete post
+app.delete('/posts/:id', (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (error, deletedPost) => {
+        if (error) {
+            res.redirect('/posts')
+        }
+
+        res.redirect('/posts')
+        console.log(deletedPost)
+    })
 })
 
 app.listen(PORT, () => {
