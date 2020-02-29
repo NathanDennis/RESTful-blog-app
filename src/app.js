1263410a9dfe2b90d3264eb43c2992fd7a706027
NathanDennis <nathdennis@googlemail.com>
2020-02-29 07:21:18 -0500

    completed styling, added express-sanitizer

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ const { PORT } = process.env
 
 // express setup
 const express = require('express')
+const expressSanitizer = require('express-sanitizer')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(expressSanitizer())
 app.use(methodOverride('_method'))
 
 // mongoose setup
@@ -26,6 +28,7 @@ const blogSchema = new mongoose.Schema({
 const Blog = mongoose.model('Blog', blogSchema)
 
 // ROUTES
+
 app.get('/', (req, res) => {
     res.redirect('/posts')
 })
@@ -43,15 +46,19 @@ app.get('/posts', (req, res) => {
 
 // create routes
 app.get('/posts/new', (req, res) => {
+
     res.render('new')
 })
 
 app.post('/posts', (req, res) => {
+    req.body.post.body = req.sanitize(req.body.post.body)
+
     Blog.create(req.body.post, (error, newPost) => {
         if (error) {
             res.render('new')
         }
 
+        console.log(`Post created: ${newPost}`)
         res.redirect('/posts')
     })
 })
@@ -79,6 +86,8 @@ app.get('/posts/:id/edit', (req, res) => {
 
 // update post
 app.put('/posts/:id', (req, res) => {
+    req.body.post.body = req.sanitize(req.body.post.body)
+
     Blog.findByIdAndUpdate(req.params.id, req.body.post, (error, updatedPost) => {
         if (error) {
             res.redirect('/posts')
